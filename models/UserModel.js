69247eb61555af1c989b3c29ae54927ae01adc71
PSPIPES\Acer <rgmadredano@gmail.com>
2022-08-11 01:56:42 +0800

    Fresh server using Mongodb atlas and local server using mongodb compass

---
+++ models/UserModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number
+  }
+}, {
+  timestamps: true
+});
+
+
+module.exports = mongoose.model('User', userSchema)
