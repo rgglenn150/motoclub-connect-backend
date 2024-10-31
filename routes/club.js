f354d1f62179bac42a26fa4b28150ed391475433
Ruben Glenn Madredano <rgmadredano@gmail.com>
2024-11-01 00:41:38 +0800

    Added middleware. - updated routes

--- routes/club.js
+++ routes/club.js
@@ -2,8 +2,11 @@ const express = require('express');
 const clubController = require('../controllers/clubController');
 const router = express.Router();
 
-router.post('/addMember', clubController.addMember);
-router.post('/create', clubController.createClub);
+import authMiddleware from '../middlewares/authMiddleware.js';
+
+
+router.post('/addMember',authMiddleware, clubController.addMember);
+router.post('/create',authMiddleware, clubController.createClub);
 
 router.get('/', clubController.getAllClubs);
 
