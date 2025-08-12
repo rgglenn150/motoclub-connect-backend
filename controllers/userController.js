70021483e5498677897bed9341d6ae0c2db337dd
Ruben Glenn Madredano <rgmadredano@gmail.com>
2025-08-12 10:35:19 +0800

    Fixed auth routes. Removed password from User fetch

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ import sharp from 'sharp';
 
 export const getUser = async (req, res) => {
   try {
-    const users = await User.find();
+    const users = await User.find().select('-password');
     res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ message: error.message });
