4591b318793e110376303a5356bfd037e16fc6f2
Ruben Glenn Madredano <rgmadredano@gmail.com>
2026-03-09 00:20:02 +0000

    fixed cors issue

--- server.js
+++ server.js
@@ -19,11 +19,13 @@ const app = express();
 
 //middleware
 app.use(express.json());
-app.use(cors());
+
+// CORS configuration
 const corsOptions = {
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
 };
 
 app.use(cors(corsOptions));
